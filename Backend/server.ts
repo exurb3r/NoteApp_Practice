fe54f9d8eb5f7331532e1f2e40401dfccab3c882
Jerick Chris Omilig <jerickomilig@gmail.com>
2026-07-15 08:50:54 +0800

    Update

--- Backend/server.ts
+++ Backend/server.ts
@@ -1,3 +1,7 @@
+import dns from "node:dns";
+dns.setServers(["8.8.8.8", "1.1.1.1"]);
+
+
 import dotenv from "dotenv";
 import express from "express";
 import mongoose from "mongoose";
--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,10 @@
 import { RouterProvider } from 'react-router-dom';
 import { router } from './routes/index.jsx'; 
 
-function App() {
+export default function App() {
   return (
   <RouterProvider router={router} />
   )
     
 
 }
-export default App;
